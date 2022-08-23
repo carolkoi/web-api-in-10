8f91de072d32dcdbd166cb43cad2f9e2db80b803
Caroline Wangui Gitonga <cwgitonga@safaricom.co.ke>
2022-08-23 14:30:34 +0300

    Initial Commit

---
+++ Models/Apiin10Context.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApiIn10Mins.Models
+{
+    
+    /// <summary>
+    /// Scaffold-DbContext "Server=localhost;Port=3306;Database=sakila;Uid=myusername;
+    /// Pwd=mypwd;" MySql.Data.EntityFrameworkCore -OutputDir Models
+    /// </summary>
+    public partial class Apiin10Context : DbContext
+    {
+        public Apiin10Context()
+        {
+        }
+
+        public Apiin10Context(DbContextOptions<Apiin10Context> options)
+            : base(options)
+        {
+        }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+                
+////#warning
+////To protect potentially sensitive information in your connection string, you should move it out of source code.
+////See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseMySQL("Server=localhost;Port=3306;Database=Apiin10;Uid=root;Pwd=;");
+//            }
+//        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
